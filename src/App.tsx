483bbbcb0e6cd575e8916401b90c366ed29f3055
SOSAAX <65075859+SOSAAX@users.noreply.github.com>
2025-12-31 03:20:52 -0500

    Refactor App component and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,24 @@
-// src/App.tsx
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-
 import { CartProvider } from "@/context/CartContext";
 
-import Index from "./pages/Index";
-import Services from "./pages/Services";
-import FleetContracts from "./pages/FleetContracts";
-import Parts from "./pages/Parts";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "@/pages/Home";
+import Parts from "@/pages/Parts";
+import Cart from "@/pages/Cart";
+import Checkout from "@/pages/Checkout";
+import CheckoutSuccess from "@/pages/CheckoutSuccess";
 
-// These pages must exist (we’ll do them next if you don’t have them yet)
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import CheckoutSuccess from "./pages/CheckoutSuccess";
-
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <CartProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/fleet-contracts" element={<FleetContracts />} />
-              <Route path="/parts" element={<Parts />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-
-              {/* Cart + Stripe checkout flow */}
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/checkout-success" element={<CheckoutSuccess />} />
-
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </HelmetProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/parts" element={<Parts />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout-success" element={<CheckoutSuccess />} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
+  );
+}
